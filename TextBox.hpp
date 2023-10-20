c01ce085e9c34aea5aa9301312ad1b196ff2291a
NoCopilot <145158819+NoCopilot@users.noreply.github.com>
2023-10-20 06:58:06 +0200

    Add files via upload

--- TextBox.hpp
+++ TextBox.hpp
@@ -1,128 +1,125 @@
-#ifndef TEXTBOX_HPP
+#ifndef TEXTBOX_HPP
 #define TEXTBOX_HPP
-
-#include "SFML/Graphics.hpp"
-#include <vector>
+
+#include "SFML/Graphics.hpp"
+#include <vector>
 #include <iostream>
-
-class TextBox
-{
-	private:
-		bool multiple_lines;
-		std::vector<sf::String> text;
-		int px, py;
-		int select_begin, select_end;
-		sf::Font font;
+
+class TextBox
+{
+	private:
+		bool multiple_lines;
+		std::vector<sf::String> text;
+		int px, py;
+		int select_begin, select_end;
+		sf::Font font;
 		sf::Text toDraw;
-		int line_height;
+		int line_height;
 		
-		sf::RenderWindow* win;
-		sf::View view;
-		float x, y, w, h;
+		sf::RenderWindow* win;
+		sf::View view;
+		float x, y, w, h;
 	public:
-		void setUp(sf::RenderWindow& window, sf::Vector2f size, float top, float left, float bottom, float right)
-		{
-			win = &window;
-			view = win->getView();
-			view.setViewport(sf::FloatRect(left / view.getSize().x, top / view.getSize().y, 
-				(view.getSize().x - right) / view.getSize().x - left / view.getSize().x, 
-				(view.getSize().y - bottom) / view.getSize().y - top / view.getSize().y));
-			view.setSize(size);
-			
-			multiple_lines = false;
-			px = 0;
-			py = 0;
-			text.push_back("");
-			
-			toDraw.setFillColor(sf::Color::White);
-			line_height = toDraw.getCharacterSize() + 10;
-			x = left; y = top; w = right; h = bottom;
-		}
+		void setUp(sf::RenderWindow& window, float top, float left, float width, float height)
+		{
+			win = &window;
+			x = left; y = top; w = width; h = height;
+			view.reset(sf::FloatRect(0, 0, width, height));
+			view.setViewport(sf::FloatRect(left / win->getSize().x, top / win->getSize().y, 
+				(width) / win->getSize().x, (height) / win->getSize().y));
+			
+			multiple_lines = false;
+			px = 0;
+			py = 0;
+			text.push_back("");
+			
+			toDraw.setFillColor(sf::Color::Black);
+			line_height = toDraw.getCharacterSize() + 10;
+		}
 		void listen(sf::Event& e)
 		{
-			if(e.type == sf::Event::KeyPressed)
-			{
-				switch(e.key.code)
+			if(e.type == sf::Event::KeyPressed)
+			{
+				if(e.key.code == sf::Keyboard::Delete)
 				{
-					case sf::Keyboard::Delete:
-					{
-						break;
-					}
-					case sf::Keyboard::Up:
-					{
-						if(py > 0)
-						{
-							py--;
-							if(px > text[py].getSize())
-								px = text[py].getSize();
-						}
-						break;
-					}
-					case sf::Keyboard::Down:
-					{
-						if(py < text.size() - 1)
-						{
-							py++;
-							if(px > text[py].getSize())
-								px = text[py].getSize();
-						}
-						break;
-					}
-					case sf::Keyboard::Left:
-					{
-						if(px > 0) px--;
-						else if(py > 0)
-						{
-							py--;
-							px = text[py].getSize();
-						}
-						break;
-					}
-					case sf::Keyboard::Right:
-					{
-						if(px < text[py].getSize()) px++;
-						else if(py < (text.size() - 1))
-						{
-							px = 0;
-							py++;
-						}
-						break;
-					}
-					
+					return;
 				}
-			}
+				if(e.key.code == sf::Keyboard::Up)
+				{
+					if(py > 0)
+					{
+						py--;
+						if(px > (int)text[py].getSize())
+							px = text[py].getSize();
+					}
+					return;
+				}
+				if(e.key.code == sf::Keyboard::Down)
+				{
+					if(py < text.size() - 1)
+					{
+						py++;
+						if(px > (int)text[py].getSize())
+							px = text[py].getSize();
+					}
+					return;
+				}
+				if(e.key.code == sf::Keyboard::Left)
+				{
+					if(px > 0) px--;
+					else if(py > 0)
+					{
+						py--;
+						px = text[py].getSize();
+					}
+					
+					return;
+				}
+				if(e.key.code == sf::Keyboard::Right)
+				{
+					if(px < (int)text[py].getSize()) px++;
+					else if(py < (int)(text.size() - 1))
+					{
+						px = 0;
+						py++;
+					}
+					
+					return;
+				}
+			}
 			if(e.type == sf::Event::TextEntered)
 			{
 				switch(e.text.unicode)
 				{
-					//copy
 					case 3:
 					{
+						//copy
 						break;
 					}
-					//copy + delete selected text
 					case 22:
 					{
+						//copy + delete selected text
 						break;
 					}
-					//paste
 					case 24:
 					{
+						//paste
 						break;
 					}
-					//new line
 					case 13:
 					{
+						//new line
+						if(!multiple_lines) break;
 						text.insert(text.begin() + py + 1, text[py].substring(px));
 						text[py] = text[py].substring(0, px);
 						py++;
 						px = 0;
 						break;
 					}
-					//delete
 					case 8:
 					case 127:
 					{
+						//delete
 						if(px > 0) text[py].erase(--px);
 						else if(py > 0)
 						{
@@ -131,39 +128,57 @@ class TextBox
 						}
 						break;
 					}
-					//add character
 					default:
+						//add character
 						text[py].insert(px++, e.text.unicode);
 				}
 			}
 		}
 		void draw()
 		{
-			sf::View temp = win->getView();
-			win->setView(view);
-			
-			for(int i=0; i < (int)text.size(); i++)
-			{
-				toDraw.setString(text[i]);
-				toDraw.setPosition({0, i * line_height});
-				win->draw(toDraw);
-			}
-			
+			sf::View temp = win->getView();
+			win->setView(view);
+			
+			if(multiple_lines) multiLinesRender();
+			else singleLinesRender();
+			
 			win->setView(temp);
 		}
-		void setFont(sf::String path)
+		void singleLinesRender()
 		{
-			font.loadFromFile(path);
-			toDraw.setFont(font);
+			toDraw.setString(text[0]);
+			toDraw.setPosition(0, 0);
+			win->draw(toDraw);
 		}
-		void setChSize(unsigned short int size)
+		void multiLinesRender()
 		{
-			toDraw.setCharacterSize(size);
-			line_height = size + 10;
+			int n = (view.getCenter().y - view.getSize().y/2)/line_height;
+			int i = n;
+			n = (view.getCenter().y + view.getSize().y/2)/line_height;
+			for(; i<n && i<text.size(); ++i)
+			{
+				toDraw.setString(text[i]);
+				toDraw.setPosition(0, i*line_height);
+				win->draw(toDraw);
+			}
 		}
-		void write(sf::String toWrite)
+		void setFont(sf::String path)
+		{
+			font.loadFromFile(path);
+			toDraw.setFont(font);
+		}
+		void setChSize(unsigned short int size)
+		{
+			toDraw.setCharacterSize(size);
+			line_height = size + 10;
+		}
+		void write(sf::String toWrite)
+		{
+			return;
+		}
+		void setMultiLines(bool b)
 		{
-			
+			multiple_lines = b;
 		}
 };
 
